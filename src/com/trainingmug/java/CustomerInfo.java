66abfe5464315dda78a24de8e00f2624cc1460c0
sumanth <gowllasumanthreddy2328@gmail.com>
2024-06-23 12:06:42 +0530

    Initial commit

---
+++ src/com/trainingmug/java/CustomerInfo.java
@@ -0,0 +1,45 @@
+package com.trainingmug.java;
+
+public class CustomerInfo {
+    public static void main(String[] args) {
+
+
+        /*
+        working with primitives
+         */
+
+
+        byte age;
+         age =12;
+        System.out.println("age :"+ age);
+
+
+
+        int employeesal;
+        employeesal =900000;
+        System.out.println("order value : "+ employeesal);
+
+        long AnnualSalary;
+        AnnualSalary = 972649261843l;
+        System.out.println("AnnualSalary:"+AnnualSalary);
+
+        float AvgSalary;
+        AvgSalary= 34577.84f;
+        System.out.println("AvgSalary: "+ AvgSalary);
+
+        double AverageAnnualRevenue;
+        AverageAnnualRevenue = 776865.978;
+        System.out.println("AverageAnnualRevenue:"+AverageAnnualRevenue);
+
+        char gender;
+        gender='f';
+        System.out.println("gender:"+gender);
+
+
+        boolean isEligible;
+        isEligible = false;
+        System.out.println("isEligible:"+isEligible);
+    }
+}
+
+
